7480f95039b80c42341b75a85829c85e7ceadb32
st-doval17 <131451577+st-doval17@users.noreply.github.com>
2023-05-03 16:51:16 +0200

    Changes to scripts.js
    -clear file to begin project
    -create variable pokemonList
    -update code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert("Hello world");
-
-let favoriteFood = "tacos";
-document.write(favoriteFood);
+let pokemonList = [
+	{
+		name: "Charmander",
+		height: 6,
+		types: ["monster", "dragon"],
+	},
+	{
+		name: "Squirtle",
+		height: 5,
+		types: ["monster", "water1"],
+	},
+	{
+		name: "Slowbro",
+		height: 160,
+		types: ["monster", "water1"],
+	},
+];
