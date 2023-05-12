bae0e0a811e42c2d8879371416172137f9812bbd
st-doval17 <131451577+st-doval17@users.noreply.github.com>
2023-05-12 16:02:20 +0200

    code changes
    -removal of code lines 34 through 54 that were repeats

--- js/scripts.js
+++ js/scripts.js
@@ -31,28 +31,6 @@ let pokemonRepository = (function () {
 	};
 })();
 
-let pokemonList = [
-	{
-		name: "Charmander",
-		height: 0.6,
-		types: ["monster", "dragon"],
-	},
-	{
-		name: "Squirtle",
-		height: 0.5,
-		types: ["monster", "water1"],
-	},
-	{
-		name: "Slowbro",
-		height: 1.6,
-		types: ["monster", "water1"],
-	},
-];
-
-pokemonList.forEach(function (pokemon) {
-	pokemonRepository.getAll(pokemon);
-});
-
 pokemonRepository.getAll().forEach(function (pokemon) {
 	document.write("<p>Name: " + pokemon.name + "</p>");
 	document.write("<p>Height: " + pokemon.height + "</p>");
