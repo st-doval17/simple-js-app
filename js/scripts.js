3e9dc837aa534c3c8e290d6242a247f2f093a3bb
st-doval17 <131451577+st-doval17@users.noreply.github.com>
2023-05-16 17:03:31 +0200

    change to js and css
    -add event listener
    -create button element
    -styling to css

--- js/scripts.js
+++ js/scripts.js
@@ -17,22 +17,50 @@ let pokemonRepository = (function () {
 		},
 	];
 
-	function add(pokemon) {
-		pokemonList.push(pokemon);
-	}
-
 	function getAll() {
 		return pokemonList;
+	}
+
+	function add(pokemon) {
+		if (
+			typeof pokemon === "object" &&
+			"name" in pokemon &&
+			"height" in pokemon &&
+			"types" in pokemon
+		) {
+			pokemonList.push(pokemon);
+		} else {
+			console.log("failed to add pokemon");
+		}
+	}
+
+	function addListItem(pokemon) {
+		let pokemonList = document.querySelector(".pokemon-list");
+		let listPokemon = document.createElement("li");
+		let button = document.createElement("button");
+		button.innerText = pokemon.name;
+		button.classList.add("button-class");
+		listPokemon.appendChild(button);
+		pokemonList.appendChild(listPokemon);
+		button.addEventListener("click", () => showDetails(pokemon));
+	}
+
+	function showDetails(pokemon) {
+		console.log(pokemon);
 	}
 
 	return {
 		add: add,
 		getAll: getAll,
+		addListItem: addListItem,
 	};
 })();
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-	document.write("<p>Name: " + pokemon.name + "</p>");
-	document.write("<p>Height: " + pokemon.height + "</p>");
-	document.write("<p>Type: " + pokemon.types.join(", ") + "</p>");
+	pokemonRepository.addListItem(pokemon);
 });
+
+let mainTitle = document.querySelector("h1");
+console.log(mainTitle.innerText);
+mainTitle.innerText = "List of Pokemon";
+console.log(mainTitle.innerText);
